3c33484d7cb12e5caf6abcba910aecbbf1a6fcb2
Leo Sutic <leo@sutic.nu>
2021-03-30 11:11:06 +0200

    Format test code.

--- astyle.cmd
+++ astyle.cmd
@@ -1,7 +1,8 @@
 @SETLOCAL
 @ECHO OFF
 
-"C:\Program Files\AStyle\bin\astyle.exe" --style=google --indent=spaces=4 --recursive src/main/cpp/*.cpp src/main/cpp/*.hpp
+"C:\Program Files\AStyle\bin\astyle.exe" --style=google --indent=spaces=4 --recursive src/main/cpp/*.cpp src/main/cpp/*.hpp src/test/cpp/*.cpp src/test/cpp/*.hpp
 del /S src\main\cpp\*.orig
+del /S src\test\cpp\*.orig
 
 @ENDLOCAL
